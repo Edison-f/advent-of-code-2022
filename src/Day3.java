40ec443104a1ab062c0a5b70a767f62a676470d7
Edison-f <osgoodbacon@gmail.com>
2022-12-03 17:24:19 -0800

    Finish day 3 part 2

--- src/Day3.java
+++ src/Day3.java
@@ -1,2 +1,34 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Day3 {
+    public static void main(String[] args) throws FileNotFoundException {
+        FileInputStream fs = new FileInputStream("./src/day3.txt");
+        Scanner in = new Scanner(fs);
+        String[] next = new String[3];
+        int result = 0;
+        while(in.hasNext()) {
+            for(int i = 0; i < 3; i++) {
+                next[i] = in.next();
+            }
+
+            for (int i = 0; i < next[0].length(); i++) {
+                char curr = next[0].charAt(i);
+                if(next[1].indexOf(curr) != -1 && next[2].indexOf(curr) != -1) {
+                    if(Character.toLowerCase(curr) == curr) {
+                        result += curr - 96;
+                        next[0] = next[0].replaceAll(curr + "", "");
+                        System.out.println(curr - 98 + " " + curr + " " + i);
+                    } else {
+                        result += curr - 38;
+                        next[0] = next[0].replaceAll(curr + "", "");
+                        System.out.println(curr - 38 + " " + curr + " " + i);
+                    }
+                }
+            }
+        }
+        System.out.println(result);
+    }
 }
