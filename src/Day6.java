3b56d150111c43fe15e1ac24752230df7eacd3bc
Edison-f <osgoodbacon@gmail.com>
2022-12-06 11:17:58 -0800

    Finish day 6 part 2

--- src/Day6.java
+++ src/Day6.java
@@ -1,5 +1,6 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Day6 {
@@ -7,18 +8,14 @@ public class Day6 {
         FileInputStream fs = new FileInputStream("./src/day6.txt");
         Scanner in = new Scanner(fs);
         String input = in.next();
-        for (int i = 0; i < input.length() - 4; i++) {
-            char first = input.charAt(i);
-            char second = input.charAt(i + 1);
-            char third = input.charAt(i + 2);
-            char fourth = input.charAt(i + 3);
-            if (first != second && first != third && first != fourth) {
-                if(second != third && second != fourth) {
-                    if(third != fourth) {
-                        System.out.print(i + 4);
-                        break;
-                    }
-                }
+        for (int i = 0; i < input.length() - 14; i++) {
+            HashSet<Character> set = new HashSet<>();
+            for (int j = i; j < i + 14; j++) {
+                set.add(input.charAt(j));
+            }
+            if(set.size() == 14) {
+                System.out.print(i + 14);
+                break;
             }
         }
     }
