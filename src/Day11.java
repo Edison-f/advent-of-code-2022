a581c1db25b3407f1c897f8845fcbbc4f7c82959
Edison-f <osgoodbacon@gmail.com>
2022-12-15 13:12:28 -0800

    Finish day 11 part 2, LCM solution

--- src/Day11.java
+++ src/Day11.java
@@ -1,30 +1,24 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Day11 {
+
+    static int lcm = 1;
+
     public static void main(String[] args) throws FileNotFoundException {
-
-//        ArrayInteger a1 = new ArrayInteger(3);
-//        ArrayInteger a2 = new ArrayInteger(8);
-//        a1.times(a2);
-//        System.out.print(a1);
-//        if(true)
-//            return;
-
         FileInputStream fs = new FileInputStream("./src/day11.txt");
         Scanner in = new Scanner(fs);
         ArrayList<Monkey> monkeyArray = new ArrayList<>();
 
         while(in.hasNextLine()) {
-            long id = Integer.parseInt(in.nextLine().split(" ")[1].charAt(0) + "");
+            int id = Integer.parseInt(in.nextLine().split(" ")[1].charAt(0) + "");
             String[] itemStrings = in.nextLine().trim().split(" ", 3)[2].split(", ");
-            ArrayList<ArrayInteger> items = new ArrayList<>();
+            ArrayList<Integer> items = new ArrayList<>();
             for (String s :
                     itemStrings) {
-                items.add(new ArrayInteger(Integer.parseInt(s)));
+                items.add(Integer.parseInt(s));
             }
             String operationString = in.nextLine().trim();
             String[] operands = new String[] {operationString.split(" ")[3], operationString.split(" ")[5]};
@@ -37,13 +31,24 @@ public class Day11 {
                 in.nextLine();
             }
         }
+        lcm = lcm(monkeyArray.get(0).divisor, monkeyArray.get(1).divisor);
+        for (int i = 2; i < monkeyArray.size(); i++) {
+            lcm(monkeyArray.get(i).divisor, lcm);
+        }
+        int mult = 1;
+        for (Monkey m :
+                monkeyArray) {
+            mult *= m.divisor;
+        }
 
-        long roundCount = 20;
-        for (long i = 0; i < roundCount; i++) {
+        lcm = mult;
+
+        int roundCount = 10000;
+        for (int i = 0; i < roundCount; i++) {
             for (Monkey monkey :
                     monkeyArray) {
                 while(!monkey.items.isEmpty()) {
-                    ArrayInteger inspected = monkey.inspect();
+                    int inspected = monkey.inspect();
                     if(monkey.isDivisible(inspected)) {
                         monkeyArray.get(monkey.throwTargets[0]).items.add(inspected);
                     } else {
@@ -51,39 +56,47 @@ public class Day11 {
                     }
                 }
             }
-            if(i == 19) {
-                for (Monkey monkey:
-                     monkeyArray) {
-                    System.out.print(monkey.id + ": " + monkey.inspectCounter + "\t");
-                }
-            }
         }
 
-        ArrayInteger first = new ArrayInteger(0);
-        ArrayInteger second = new ArrayInteger(0);
+        int first = 0;
+        int second = 0;
         for (Monkey monkey :
                 monkeyArray) {
-            if(monkey.inspectCounter.greaterThan(first)) {
+            if(monkey.inspectCounter > first) {
                 second = first;
                 first = monkey.inspectCounter;
-            } else if(!monkey.inspectCounter.greaterThan(second)) {
+            } else if(monkey.inspectCounter > second) {
                 second = monkey.inspectCounter;
             }
         }
-        first.times(second);
-        System.out.println(first);
+        System.out.println((long) first* (long) second);
+    }
+
+    public static int lcm(int n1, int n2) {
+        int max = Math.max(n1, n2);
+        int min = Math.min(n1, n2);
+        if(min == 0) {
+            return max;
+        }
+        int mult = min;
+        int count = 0;
+        while(mult < max) {
+            mult += min;
+            count++;
+        }
+        return lcm(min, max - (min * Math.max(count, 1)));
     }
 
     public static class Monkey {
-        public ArrayList<ArrayInteger> items;
+        public ArrayList<Integer> items;
         public char operator;
         public String[] operands;
         public int divisor;
-        public long id;
+        public int id;
         public int[] throwTargets;
-        public ArrayInteger inspectCounter;
+        public int inspectCounter;
 
-        public Monkey(ArrayList<ArrayInteger> items, char operator, String[] operands, int divisor, long id, int[] throwTargets) {
+        public Monkey(ArrayList<Integer> items, char operator, String[] operands, int divisor, int id, int[] throwTargets) {
             this.items = items;
             this.operator = operator;
             this.operands = operands;
@@ -91,147 +104,42 @@ public class Day11 {
             this.id = id;
             this.throwTargets = throwTargets;
 
-            inspectCounter = new ArrayInteger(0);
+            inspectCounter = 0;
         }
 
-        public ArrayInteger inspect() {
-            inspectCounter.add(new ArrayInteger(1));
-            ArrayInteger item = items.remove(0);
+        public int inspect() {
+            inspectCounter++;
+            int item = items.remove(0) % lcm;
+            boolean first = operands[0].equals("old");
             boolean second = operands[1].equals("old");
-            if(second) {
+            if(first && second) {
                 if(operator == '*') {
-                    item.times(item);
+                    item = powReduce(item);
                 } else {
-                    item.times(new ArrayInteger(2));
+                    item *= 2;
                 }
             } else {
                 if(operator == '*') {
-                    item.times(new ArrayInteger(Integer.parseInt(operands[1])));
+                    item *= Integer.parseInt(operands[1]);
                 } else {
-                    item.add(new ArrayInteger(Integer.parseInt(operands[1])));
+                    item += Integer.parseInt(operands[1]);
                 }
             }
-            return item;
+            return item % lcm;
         }
 
-        public boolean isDivisible(ArrayInteger n) {
-            return n.divisibleBy(divisor);
-        }
-    }
-
-    public static class ArrayInteger {
-        public ArrayList<Integer> list;
-
-        public ArrayInteger(int initVal) {
-            list = new ArrayList<>();
-            createList(initVal);
-        }
-
-        public ArrayInteger(ArrayList<Integer> arr) {
-            list = new ArrayList<>();
-            list.addAll(arr);
-        }
-
-        public void createList(int initVal) {
-            String intString = initVal + "";
-            for (int i = 0; i < intString.length(); i++) {
-                list.add(Integer.parseInt(intString.substring(i, i + 1)));
-            }
-        }
-
-        @Override
-        public String toString() {
-            String result = "";
-            for (int i :
-                    list) {
-                result += i + "";
+        public int powReduce(int n) {
+            int init = n;
+            int result = 0;
+            for(int i = 0; i < n; i++) {
+                result += init;
+                result = result % lcm;
             }
             return result;
         }
 
-        public void add(ArrayInteger n) {
-
-            ArrayInteger copy = n;
-            while(copy.list.size() < list.size()) {
-                copy.list.add(0, 0);
-            }
-            for (int i = 0; i < copy.list.size(); i++) {
-                if(i == list.size()) {
-                    list.add(0);
-                }
-                list.set(i, copy.list.get(i) + list.get(i));
-            }
-            carry();
-        }
-
-        public void times(ArrayInteger n) {
-            ArrayInteger one = new ArrayInteger(1);
-            ArrayInteger copy = new ArrayInteger(list);
-            for(ArrayInteger i = new ArrayInteger(1); n.greaterThan(i); i.add(one)) {
-                add(copy);
-            }
-        }
-
-        public void carry() {
-            for (int i = 0; i < list.size(); i++) {
-                if(list.get(i) >= 10) {
-                    list.set(i, list.get(i) - 10);
-                    if(i == 0) {
-                        list.add(0, 0);
-                        list.set(0, list.get(0) + 1);
-                    } else {
-                        list.set(i - 1, list.get(i - 1) + 1);
-                    }
-                }
-            }
-            if(needsCarry()) {
-                carry();
-            }
-        }
-
-        public boolean needsCarry() {
-            for (int i :
-                    list) {
-                if (i >= 10) {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        public boolean greaterThan(ArrayInteger n) {
-            if(n.list.size() < list.size()) {
-                return true;
-            } else if(n.list.size() > list.size()) {
-                return false;
-            }
-            for (int i = 0; i < list.size(); i++) {
-                if(list.get(i) > n.list.get(i)) {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        public boolean equals(ArrayInteger n) {
-            if(n.list.size() != list.size()) {
-                return false;
-            }
-            for (int i = 0; i < list.size(); i++) {
-                if(list.get(i) != n.list.get(i)) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        public boolean divisibleBy(int n) {
-            ArrayInteger base = new ArrayInteger(n);
-            ArrayInteger curr = new ArrayInteger(n);
-            while(greaterThan(curr)) {
-                curr.add(base);
-            }
-            return equals(curr);
+        public boolean isDivisible(int n) {
+            return (n % divisor == 0);
         }
     }
 }
